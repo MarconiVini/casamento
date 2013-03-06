7127eed7af1a9b6c3240435ddb144e10eb342e02
Marcos Vinicius <cardosounicamp@gmail.com>
2013-03-06 07:48:11 -0300

    Ajuste do select name para o Heroku

--- app/controllers/site/presence_controller.rb
+++ app/controllers/site/presence_controller.rb
@@ -7,13 +7,13 @@ class Site::PresenceController < ApplicationController
   def members
     @all = Member.all
     @query = params[:presence][:name]
-    @members = Member.find(:all, conditions: ["name like ?", "%#{params[:presence][:name]}%"])
+    @members = Member.find(:all, conditions: ["lower(name) like ?", "%#{params[:presence][:name]}%"])
   end
   
   def family
     param = params[:parametrize_name]
     parametrize = param.gsub("-", " ").downcase
-    @member_from_url = Member.find(:first, conditions: ["name like ?", "%#{parametrize}%"]); 
+    @member_from_url = Member.find(:first, conditions: ["lower(name) like ?", "%#{parametrize}%"]); 
     @family = Family.find(@member_from_url.family_id)
     @members = Member.find(:all, conditions: ["family_id == ?", "#{@member_from_url.family_id}"])
   end
@@ -21,7 +21,7 @@ class Site::PresenceController < ApplicationController
   def confirm
     param = params[:parametrize_name]
     parametrize = param.gsub("-", " ").downcase
-    @member_from_url = Member.find(:first, conditions: ["name like ?", "%#{parametrize}%"]); 
+    @member_from_url = Member.find(:first, conditions: ["lower(name) like ?", "%#{parametrize}%"]); 
     @member_from_url.confirmado = true    
     @member_from_url.save
     redirect_to family_path(param)
@@ -31,7 +31,7 @@ class Site::PresenceController < ApplicationController
   def cancel
     param = params[:parametrize_name]
     parametrize = param.gsub("-", " ").downcase
-    @member_from_url = Member.find(:first, conditions: ["name like ?", "%#{parametrize}%"]); 
+    @member_from_url = Member.find(:first, conditions: ["lower(name) like ?", "%#{parametrize}%"]); 
     @member_from_url.confirmado = false    
     @member_from_url.save
     redirect_to family_path(param)
--- config.ru
+++ config.ru
@@ -2,3 +2,4 @@
 
 require ::File.expand_path('../config/environment',  __FILE__)
 run PersonalTrainer::Application
+$stdout.sync = true
